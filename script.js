1e3ca2eac2fbbe386116ed7398a05273a8f5bf63
David Whitney <david@davidwhitney.co.uk>
2021-09-15 00:43:44 +0100

    Add a click handler to the add button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const taskInput = document.getElementById("new");
 const addButton = document.getElementById("add");
 const tasks = document.getElementById("tasks");
+
+addButton.addEventListener('click', addNewItem);
+
+
+function addNewItem() {
+    console.log("Add new item!");
+}
